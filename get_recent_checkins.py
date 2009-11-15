3b196d516e6dfed81cc0071f089d8fe155a0fa84
Steven <lehrburger@gmail.com>
2009-11-15 14:59:10 -0500

    fixed zipcode and other stuff, basic functionality working again, time for maps

--- get_recent_checkins.py
+++ get_recent_checkins.py
@@ -8,42 +8,53 @@ import logging
 def fetch_and_store_n_recent_checkins_for_token(token, limit, client):
   response = client.make_request("http://api.foursquare.com/v1/history.json", token = token.token, secret = token.secret, additional_params = {'l':limit})
   #ad7zy4hiv4yi temp random password
-  try:
-    history = json.loads(response.content)
-    logging.debug(history)
-    for checkin in history['checkins']:
-      if 'venue' in checkin:
-        if Venue.all().filter('venue_id =', checkin['venue']['id']).count() == 0:
-          # if we don't have the venue yet, add it
-          venue = Venue()
-          venue.venue_id    = checkin['venue']['id']
-          venue.name        = checkin['venue']['name']
-          venue.address     = checkin['venue']['address']
-          venue.crossstreet = checkin['venue']['crossstreet']
-          venue.city        = checkin['venue']['city']
-          venue.state       = checkin['venue']['state']
-          venue.zipcode     = checkin['venue']['zip']
-          venue.geolat      = checkin['venue']['geolat']
-          venue.geolong     = checkin['venue']['geolong']
-          venue.phone       = checkin['venue']['phone']
-          venue.put()
-        else:
-          # otherwise grab it
-          venue = Venue.all().filter('venue_id =', checkin['venue']['id']).get()
+  # try:
+  history = json.loads(response.content)
+  logging.debug(history)
+  for checkin in history['checkins']:
+    if 'venue' in checkin:
+      json_venue = checkin['venue']
+      if 'id' in json_venue and Venue.all().filter('venue_id =', json_venue['id']).count() == 0:
+        # if we don't have the venue yet, add it
+        venue = Venue()
+        venue.venue_id    = checkin['venue']['id']
 
-        if Checkin.all().filter('checkin_id =', checkin['id']).count() == 0:
-          # if it's a new checkin, put it in the database
-          new_checkin = Checkin()
-          new_checkin.user       = token.owner
-          new_checkin.checkin_id = checkin['id']
-          new_checkin.created    = datetime.datetime.strptime(checkin['created'], "%a, %d %b %y %H:%M:%S +0000")
-          new_checkin.venue      = venue
-          logging.debug(new_checkin)
-          new_checkin.put()
-        # else
-          #it's already there and we're good
-  except:
-    logging.error("There was a problem with the response: " % response)
+        if 'name' in json_venue:
+          venue.name        = json_venue['name']
+        if 'address' in json_venue:
+          venue.address     = json_venue['address']
+        if 'crossstreet' in json_venue:
+          venue.crossstreet = json_venue['crossstreet']
+        if 'city' in json_venue:
+          venue.city        = json_venue['city']
+        if 'state' in json_venue:
+          venue.state       = json_venue['state']
+        if 'zip' in json_venue:
+          venue.zipcode     = int(json_venue['zip'])
+        if 'geolat' in json_venue:
+          venue.geolat      = json_venue['geolat']
+        if 'geolong' in json_venue:
+          venue.geolong     = json_venue['geolong']
+        if 'phone' in json_venue:
+          venue.phone       = json_venue['phone']
+        venue.put()
+      else:
+        # otherwise grab it
+        venue = Venue.all().filter('venue_id =', json_venue['id']).get()
+
+      if Checkin.all().filter('checkin_id =', checkin['id']).count() == 0:
+        # if it's a new checkin, put it in the database
+        new_checkin = Checkin()
+        new_checkin.user       = token.owner
+        new_checkin.checkin_id = checkin['id']
+        new_checkin.created    = datetime.datetime.strptime(checkin['created'], "%a, %d %b %y %H:%M:%S +0000")
+        new_checkin.venue      = venue
+        logging.debug(new_checkin)
+        new_checkin.put()
+      # else
+        #it's already there and we're good
+  # except:
+  #   logging.error("There was a problem with the response: " + response.content)
 
 
 consumer_key = "98ff47ad5541ebaaee51cb5a1e843d1404aeba03f"
@@ -56,7 +67,7 @@ all_tokens = Token.all().order('-created').fetch(1000)
 user_list = []
 for token in all_tokens:
   if token.owner in user_list:
-    token.delete()
+    token.delete() # delete extra older tokens for each user
   else:
     user_list.append(token.owner)
     fetch_and_store_n_recent_checkins_for_token(token, 20, client)
