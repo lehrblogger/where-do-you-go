9d1f3970f95e794345d85f7c2d0625485f049de2
Steven <lehrburger@gmail.com>
2009-12-12 21:15:09 -0500

    removing commented code from dot.py

--- gheatae/dot.py
+++ gheatae/dot.py
@@ -1,20 +1,6 @@
 import math
 import pngcanvas
 
-#def createDot(rad, max_alpha = 100):
-#  img = pngcanvas.PNGCanvas(int(rad * 2), int(rad * 2))
-#  for y in range(0, int(rad * 2)):
-#    for x in range(0, int(rad * 2)):
-#      y_adj = math.pow(y - rad, 2)
-#      x_adj = math.pow(x - rad, 2)
-#      pt_rad = math.sqrt(y_adj + x_adj)
-#      if pt_rad > rad:
-#        img.canvas[y][x] = [ 0, 0, 0, 0]
-#        continue
-#      alpha = int(max_alpha * ((rad - pt_rad) / rad))
-#      img.canvas[y][x] = [ 0, 0, 0, alpha]
-#  return img
-
 def createDot(rad, max_alpha = 100):
   level = []
   for i in range(int(rad * 2)):
@@ -33,4 +19,4 @@ def createDot(rad, max_alpha = 100):
 
 dot = []
 for i in range(1, 31):
-  dot.append(createDot(2 * i))
+  dot.append(createDot(1.5 * i))
