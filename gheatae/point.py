744055bcf83e8e442631703bd0332148d4fa6dbd
Steven <lehrburger@gmail.com>
2009-12-16 16:53:55 -0500

    recovered project i think back to previous state, going to try on GAE

--- gheatae/point.py
+++
@@ -1,9 +0,0 @@
-from geo.geomodel import GeoModel
-from google.appengine.ext import db
-
-class DataPoint(GeoModel):
-  time = db.DateTimeProperty()
-  weight = db.IntegerProperty()
-  range = db.IntegerProperty()
-
-#TODO add user stuff here so we dont display everyone's poitns on the map, or integrate into Checkins
